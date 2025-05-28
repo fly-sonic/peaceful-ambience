c8c7fbc1ad3fe618e68876fe4cdf40ba5cbc858f
fly-sonic <>
2025-05-27 23:07:31 -0400

    add glass effect to music btns

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const musicBtn = document.getElementById("music-btn");
 musicBtn.addEventListener("click", function () {
   if (music.paused) {
     music.play();
-    musicBtn.src = "media/images/melody.png";
+    musicBtn.src = "media/images/music/music-on-skyblue.svg";
   } else {
     music.pause();
-    musicBtn.src = "media/images/muted.png";
+    musicBtn.src = "media/images/music/music-off-skyblue.svg";
   }
 });
